c2322f5906a804a9a440af4c83996b7dd1f17123
arjunByldd01 <arjun@bylddcom>
2025-11-14 16:30:56 +0530

    fix: cron

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -10,35 +10,35 @@ export class CronService {
     private readonly prisma: PrismaService,
     private readonly geminiService: GeminiService,
   ) {}
-  // @Cron(CronExpression.EVERY_30_SECONDS)
-  // async handleCron() {
-  //   // console.log('Called every 30 seconds');
-  //   // try {
-  //   //   const meetingsWithoutSummary = await this.prisma.userMeeting.findMany({
-  //   //     where: {
-  //   //       summary: '',
-  //   //       status: USER_MEETING_STATUS.ENDED,
-  //   //       transcript: {
-  //   //         not: null,
-  //   //       },
-  //   //     },
-  //   //     take: 1,
-  //   //   });
-  //   //   console.log('Meetings without summary', meetingsWithoutSummary.length);
-  //   //   for (const meeting of meetingsWithoutSummary) {
-  //   //     console.log('Generating summary for meeting', meeting.id);
-  //   //     const summary = await this.geminiService.generateSummary(
-  //   //       JSON.stringify(meeting.transcript),
-  //   //     );
-  //   //     console.log('Summary generated', summary?.length);
-  //   //     // await this.prisma.userMeeting.update({
-  //   //     //   where: { id: meeting.id },
-  //   //     //   data: { summary },
-  //   //     // });
-  //   //     console.log('Meeting updated', meeting.id);
-  //   //   }
-  //   // } catch (error) {
-  //   //   console.log('Error generating summary', error);
-  //   // }
-  // }
+  @Cron(CronExpression.EVERY_2_HOURS)
+  async handleCron() {
+    console.log('Called every 30 seconds');
+    try {
+      const meetingsWithoutSummary = await this.prisma.userMeeting.findMany({
+        where: {
+          summary: '',
+          status: USER_MEETING_STATUS.ENDED,
+          transcript: {
+            not: null,
+          },
+        },
+        take: 1,
+      });
+      console.log('Meetings without summary', meetingsWithoutSummary.length);
+      for (const meeting of meetingsWithoutSummary) {
+        console.log('Generating summary for meeting', meeting.id);
+        const summary = await this.geminiService.generateSummary(
+          JSON.stringify(meeting.transcript),
+        );
+        console.log('Summary generated', summary?.length);
+        await this.prisma.userMeeting.update({
+          where: { id: meeting.id },
+          data: { summary },
+        });
+        console.log('Meeting updated', meeting.id);
+      }
+    } catch (error) {
+      console.log('Error generating summary', error);
+    }
+  }
 }
